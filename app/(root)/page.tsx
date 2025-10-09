636ab3b0e985bda930d321fcc98066eb8981b0b7
Miguel_CR <125812157+nova-cz@users.noreply.github.com>
2025-10-09 10:03:36 -0600

    implementando JSM YC Directory

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
 import SearchForm from "@/components/SearchForm";
-
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
   
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id:1 },
+    _id:1,
+    description: 'This is description',
+    image: "https://www.cio.com/wp-content/uploads/2025/02/3831192-0-93030100-1740387509-shutterstock_2482705481.jpg?quality=50&strip=all",
+    category: "Robots",
+    title: "We Robots",
+    
+  }]
 
   return (
     <>
@@ -13,6 +25,22 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
